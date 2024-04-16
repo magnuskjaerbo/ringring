fa553b04243a9d0065e5f43bd731811ed29b84f6
Magnus Kjærbo <magnusjk@gmail.com>
2024-04-16 12:03:35 +0100

    New version

--- main.pas
+++ main.pas
@@ -79,7 +79,7 @@ begin
 
    {$IFDEF Windows}
    {$endif}
-  Label2.Caption := '1.1.21';
+  Label2.Caption := '1.1.22';
   DoubleBuffered := True;
   FLastRemoteCheck := 0;
   FLastRemoteCheckInterval := 120;
